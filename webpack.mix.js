fffd87bd452b3246e91dc79ab2396241cbe198b4
Hicham SADDEK <h.saddek@serrahub.com>
2021-03-16 18:45:00 +0100

    Refactoring changed files.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').vue()
-    .postCss('resources/css/app.css', 'public/css', [
+mix.js('vue/app.js', 'public/js').vue()
+    .postCss('styles/css/app.css', 'public/css', [
         require('postcss-import'),
         require('tailwindcss'),
         require('autoprefixer'),
