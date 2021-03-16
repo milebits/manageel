5e281c847ffd70f29d4cd833491d1db9e0ed99f0
Hicham SADDEK <h.saddek@serrahub.com>
2021-03-16 18:42:42 +0100

    Adding ManageEL class to manage Continuous Version Control.

---
+++ app/ManageEL.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class ManageEL
+{
+    public const API_VERSION = '1';
+    public const WEB_VERSION = '1';
+    public const MANAGE_EL_VERSION = '1.0.0';
+    public const MANAGE_EL_BUILD = '1';
+}
