a70e5096f322670884a44e69fc3f90af2c70abca
Hicham SADDEK <h.saddek@serrahub.com>
2021-03-23 13:47:32 +0100

    Adding API auth routes file.

---
+++ routes/api/v1/auth.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Auth\API\ForgotPasswordController;
+use App\Http\Controllers\Auth\API\LoginController;
+use App\Http\Controllers\Auth\API\RegisterController;
+use Illuminate\Http\Request;
+
+Route::post('/login', [LoginController::class, 'store'])->name('login');
+Route::post('/register', [RegisterController::class, 'store'])->name('register');
+Route::post('/forgot-password', [ForgotPasswordController::class, 'store'])->name('forgot-password');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get("/user", fn(Request $request) => $request->user())->name('user');
+    Route::post("/user/confirm_email", []);
+});
