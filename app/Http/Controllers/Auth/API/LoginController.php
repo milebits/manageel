5d927af9c258323d45094ee87593d596019cb79f
Hicham SADDEK <h.saddek@serrahub.com>
2021-03-23 13:44:20 +0100

    Enhancing API login controller to be more readable and more responsive to requests.

--- app/Http/Controllers/Auth/API/LoginController.php
+++ app/Http/Controllers/Auth/API/LoginController.php
@@ -5,29 +5,70 @@ namespace App\Http\Controllers\Auth\API;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\API\LoginRequest;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
-    public function store(LoginRequest $request): string|array
+    /**
+     * @param LoginRequest $request
+     * @return JsonResponse
+     */
+    public function store(LoginRequest $request): JsonResponse
     {
         $data = collect($request->validated());
-
-        $user = User::query()->where('email', $data->get('identifier'))
-            ->orWhere('username', $data->get('identifier'))
-            ->orWhere('phone', $data->get('identifier'))->first();
-
-        if (is_null($user)) return $this->fail();
-        if (!Hash::check($data->get('password'), $user->password)) return $this->fail();
-
-        $user->tokens()->each(fn($token) => $token->delete());
-        $token = $user->createToken($data->get('device_name'))->plainTextToken;
-
-        return compact('user', 'token');
+        $user = $this->findUser($data['identifier']);
+        if (!$this->checkUser($user, $data['password'])) return $this->fail();
+        return $this->success($request, $data['device_name']);
     }
 
-    public function fail(): string
+    /**
+     * @param string $identifier
+     * @return User|Model
+     */
+    public function findUser(string $identifier): User|Model
     {
-        return back()->withErrors(['session' => 'Identifier or password is incorrect']);
+        return User::query()->where('email', $identifier)
+            ->orWhere('username', $identifier)
+            ->orWhere('phone', $identifier)->first();
+    }
+
+    /**
+     * @param User|null $user
+     * @param string $password
+     * @return bool
+     */
+    public function checkUser(?User $user, string $password): bool
+    {
+        return !is_null($user) && Hash::check($password, $user->getAttribute('password'));
+    }
+
+    /**
+     * @param Request $request
+     * @param string $device_name
+     * @return JsonResponse
+     */
+    public function success(Request $request, string $device_name = 'Application'): JsonResponse
+    {
+        $user = $request->user();
+        $user->tokens()->each(fn($token) => $token->delete());
+        $token = $user->createToken($device_name)->plainTextToken;
+
+        return response()->json([
+            'user' => $request->user(),
+            'token' => $token,
+        ]);
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function fail(): JsonResponse
+    {
+        return response()->json([
+            'user' => null, 'token' => null
+        ]);
     }
 }
